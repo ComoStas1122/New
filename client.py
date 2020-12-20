52f3179d37633df8ac97a0ce3fb689e4cd7a2d03
comostas1122 <comostas1122@gmail.com>
2020-12-20 14:25:56 +0200

    Almost Done

--- client.py
+++ client.py
@@ -5,6 +5,11 @@ import traceback
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 65432
 FORMAT = "UTF-8"
+PLAYERS_CHECK = "Too Many Players"
+OPTIONS = "You Have 3 Options"
+GAME_OVER = "GAME OVER"
+HELP = "Do You Want"
+YOU_WON = "Well Done!!"
 '''Constants'''
 
 def get_socket(*ADDR):
@@ -29,7 +34,6 @@ def recv_msg(sock: socket.socket) -> str:
     msg = ''
     while (b := sock.recv(1)) != b'\n':
         msg += b.decode(FORMAT)
-    print(msg)
     return msg
 
 
@@ -59,17 +63,86 @@ def main():
         return
     else:
         try:
-        welcome_msg = recv_msg(sock)
-        if "Too Many Players" in welcome_msg:
+            while True:
+                    welcome_msg = recv_msg(sock)
+                    print(welcome_msg)
+                    if PLAYERS_CHECK in welcome_msg:
+                        sock.close()
+                        return 0
+                    while "Press 1" in (msg := recv_msg(sock)):
+                        print(msg)
+                        choice = ''
+                        while not choice.isdigit() or choice not in ["0","1"]:
+                            choice = input()
+                        send_msg(sock, choice)
+                        for i in range(3):
+                            print(recv_msg(sock))
+                            for j in range(4):
+                                print(recv_msg(sock))
+                            answer = ""
+                            while not answer.isdigit() or answer not in ["1","2","3","4"]:
+                                answer = input("\nEnter Your Answer: ")
+                            send_msg(sock, answer)
+                    while True:
+                        if OPTIONS in msg:
+                            print(msg)
+                            stage = input("\nEnter Your Choice: ")
+                            send_msg(sock, stage)
+                            msg = recv_msg(sock)
+                        else: break
+                    msg2 = msg
+                    while True:
+                        try:
+                            if GAME_OVER not in msg2 and YOU_WON not in msg2:
+                                print(msg2)
+                                msg2 = recv_msg(sock)
+                                a_help = ""
+                                answer = ""
+                                while HELP in msg2:
+                                    print(msg2)
+                                    a_help = input("ENTER YES or NO: ")
+                                    send_msg(sock, a_help)
+                                    msg2 = recv_msg(sock)
+                                    continue
+                                if a_help.upper() == "YES":
+                                    print(msg2)
+                                    for _ in range(2):
+                                        print(recv_msg(sock))
+                                    answer = ''
+                                    while answer not in ["1", "2"]:
+                                        answer = input("\nEnter Your Answer")
+                                    send_msg(sock, answer)
+                                    msg2 = recv_msg(sock)
+                                    continue
+                                else:
+                                    print(msg2)
+                                    for _ in range(4):
+                                        print(recv_msg(sock))
+                                    while answer not in ["1", "2", "3", "4"]:
+                                        answer = input("\nEnter Your Answer")
+                                    send_msg(sock, answer)
+                                    msg2 = recv_msg(sock)
+                            else:
+                                break
+                        except:
+                            raise
+                    print(msg2)
+                    print(recv_msg(sock))
+                    answer = ""
+                    while answer.upper() not in ["YES", "NO"]:
+                        answer = input()
+                    print("YES" == answer.upper())
+                    if answer.upper() == "YES":
+                        send_msg(answer)
+                        continue
+                    else:
+                        send_msg(answer)
+                        break
+        except:
+            traceback.print_exception()
+        finally:
             sock.close()
             return 0
-        choice = ''
-        while not choice.isdigit() or choice not in ["0","1"]:
-            start_str = recv_msg(sock)
-            choice = input()
-        send_msg(sock, choice)
-        msg = recv_msg(sock)
-        
-
+            
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import socket
 import traceback
 import multiprocessing
+import time
+import re
+from random import shuffle, randrange
+
 
 '''Constants'''
 IP = socket.gethostbyname(socket.gethostname())
@@ -10,6 +14,13 @@ WELCOME_MSG = "WELCOME TO THE CHASE"
 EXIT_MSG = "GOODBYE"
 PRESS = "Press 1 to Play and 0 to Exit"
 TOO_MANY_PLAYERS = "Too Many Players are Playing Right Now, Try Again Later..."
+QUESTION = 5000
+CHOOSE = "You Have 3 Options -> 1. Level 3 with {}\t2. Level 2 with {}\t3. Level 4 with {}"
+GAME_OVER = "GAME OVER"
+WANT_HELP = "Do You Want to Use Your Help?"
+STATE = "Bitcoins -> {}\tHelping Hand -> {}\t Your Stage -> {} Chaser's Stage -> {}"
+YOU_WON  = "Well Done!! You Won : {}"
+PLAY_AGAIN = "DO You Want To Play Again?"
 '''Constants'''
 
 def send_msg(sock, *msgs):
@@ -18,6 +29,7 @@ def send_msg(sock, *msgs):
     :sock: the socket
     :msgs: messages to send
     '''
+
     print(msgs)
     for msg in msgs:
         sock.send(msg.encode() + b"\n")
@@ -48,28 +60,160 @@ def recv_msg(sock):
     while (b := sock.recv(1)) != b'\n':
         msg += b.decode(FORMAT)
     return msg
+
+
+def extract_questions():
+    with open("Questions.txt", "r") as f:
+        lines = [line.rstrip() for line in f.readlines()]
+        f.close()
+    questions = {}
+    question_re = re.compile(r"^(.*\?)")
+    answers_re = re.compile(r"\(([-\w*,\s\w]+)\)")
+    for question_phrase in lines:
+        question = question_re.search(question_phrase).group(0)
+        answers = answers_re.search(question_phrase).group(1).split(",")
+        questions[question] = answers
+    return questions
+
+def shuffle_questions(questions):
+    keys = list(questions.keys())
+    shuffle(keys)
+    return {key : questions[key] for key in keys}
+
+def send_question(c_sock, question, answers, max_range = 4):
+    print(max_range)
+    send_msg(c_sock, question)
+    enum = answers[:-1]
+    if max_range == 2:
+        enum = [answers[-1]]
+        wrong = answers[0] if answers[0] != enum[0] else answers[1]
+        enum.append(wrong)
+        print(enum)
+    for counter, answer in enumerate(enum):
+        answer = f"{counter + 1}. {answer}"
+        send_msg(c_sock, answer)
+    answer = answers.index(enum[int(recv_msg(c_sock)) - 1]) + 1
+    return answer
+
+
+def chaser_answer(): 
+    '''
+    This Function Returns if the chaser one the 75% lottery
+    :return: True if he did False if he didn't
+    '''
+
+    return randrange(1, 5) in [1, 2, 3]
+
+    
     
 
-def game(c_sock):
+def game(dict_help, count, lock, c_sock):
     '''
     This Function Handles the Game logic
     :c_sock: The socket that attached
     '''
 
-    c_sock.settimeout(10)
-    send_msg(c_sock, WELCOME_MSG, PRESS)
     try:
-        client_choice = recv_msg(c_sock)
+        while True:
+            dict_help[count][0] = 1
+            print(dict_help[count], count)
+            dict_help[count][1] = time.time()
+            questions = extract_questions()
+            correct = 0
+            send_msg(c_sock, WELCOME_MSG)
+            while not correct:
+                questions = shuffle_questions(questions)
+                send_msg(c_sock, PRESS)
+                client_choice = recv_msg(c_sock)
+                if client_choice == '0': 
+                    send_msg(c_sock, EXIT_MSG)
+                    return
+                for (question, answers), _ in zip(questions.items(), range(3)):
+                    answer = send_question(c_sock, question, answers)
+                    if answers[int(answer) - 1] == answers[-1]:
+                        correct += 1
+                dict_help[count][1] = time.time()
+            user_money = correct * QUESTION
+            chaser_stage = 0
+            while True:
+                send_msg(c_sock, CHOOSE.format(user_money, user_money*2, user_money//2))
+                client_stage = recv_msg(c_sock)
+                if client_stage == "1":
+                    client_stage = 3
+                elif client_stage == "2":
+                    client_stage = 2
+                    user_money *= 2
+                elif client_stage == "3":
+                    user_money /= 2
+                    client_stage = 4
+                else:
+                    continue
+                break
+
+            helping_hand = 1
+            q_counter = 3
+            while True:
+                max_range = 4
+                if chaser_stage == client_stage:
+                    send_msg(c_sock, GAME_OVER)
+                    send_msg(c_sock, PLAY_AGAIN)
+                    break
+                elif client_stage == 7:
+                    send_msg(c_sock, YOU_WON.format(user_money))
+                    send_msg(c_sock, PLAY_AGAIN)
+                    break
+                send_msg(c_sock, STATE.format(user_money, helping_hand, client_stage, chaser_stage))
+                if helping_hand:
+                    send_msg(c_sock, WANT_HELP)
+                    while (ans := recv_msg(c_sock).upper()) not in ["YES", "NO"]:
+                        send_msg(c_sock, WANT_HELP)
+                        continue
+                    if ans == "YES":
+                        helping_hand = 0
+                        max_range = 2
+                answers = questions[(question := list(questions.keys())[q_counter])]
+                print(answers, question)
+                answer = send_question(c_sock, question, answers, max_range)
+                if answers[int(answer)] == answers[-1]:
+                    client_stage += 1
+                if chaser_answer():
+                    chaser_stage += 1
+                q_counter += 1
+            if recv_msg(c_sock).upper() == "YES":
+                continue
+            else:
+                break
     except:
-        print("Too Much Time waiting for an answer")
-        c_sock.shutdown(2)
+        traceback.print_exc()
+    finally:
         c_sock.close()
-        return
-    if client_choice == '0':
-        send_msg(c_sock, EXIT_MSG)
-        c_sock.close()
+        dict_help[count][0] = 0
+        if lock.acquire():
+            lock.release()
         return
 
+
+def clean_up(conns, dict_help, lock, keys):
+    try:
+        lock.acquire()
+        print("1")
+        closed_keys = [key for key in dict_help if dict_help[key] == 0 or
+                       time.time() - dict_help[key][1] > 10]
+        print(keys, dict_help, closed_keys)
+        for key in closed_keys:
+            keys.append(key)
+            del dict_help[key]
+            if not conns[key][2]._closed:
+                conns[key][2].close()
+            conns[key][1].terminate()
+            del conns[key]
+        
+    except:
+        traceback.print_exc()
+    finally:
+        print(conns, dict_help)
+        lock.release()
+            
 
 def main():
     '''
@@ -85,12 +229,14 @@ def main():
             serv_sock.close()
         return
 
-
+    dict_help = multiprocessing.Manager().dict()
     conns = {}
+    lock = multiprocessing.Lock()
+    keys = [1, 2, 3]
     while True:
         c_sock, addr = serv_sock.accept()
-        delete = []
-        if len(conns) >= 3:
+        clean_up(conns, dict_help, lock, keys)
+        if len(conns) == 3:
             p = multiprocessing.Process(target = send_msg, args = (c_sock, TOO_MANY_PLAYERS))
             p.start()
             p.join()
@@ -102,12 +248,13 @@ def main():
                 traceback.print_exc()
             finally:
                 continue
-        print(f"{addr} is connected, connection number {len(conns) + 1}")
-        p = multiprocessing.Process(target = game, args = (c_sock, ))
-        conns[p] = (addr, c_sock)
+        print(f"{c_sock} is connected, connection number {len(conns) + 1}")
+        dict_help[(key := keys.pop(0))] = [1, time.time()]
+        p = multiprocessing.Process(target = game, args = (dict_help, key, lock, c_sock))
+        conns[key] = (addr, p, c_sock)
         p.start()
-            
-                
+
+
 if __name__ == "__main__":
     main()
 
